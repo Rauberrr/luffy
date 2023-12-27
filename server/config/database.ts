73301abe50bf921e18bdbf7cbd7cdd2acbdd35d4
rauber <andrerauber2006@gmail.com>
2023-12-27 11:04:05 -0300

    deploy vercel

--- server/config/database.ts
+++ server/config/database.ts
@@ -3,13 +3,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const sequelize = new Sequelize({
-  database: process.env.POSTGRES_DB,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  dialect: 'postgres'
-})
+const database: string = process.env.DATABASE
+
+const sequelize = new Sequelize(database)
 
 export default sequelize
